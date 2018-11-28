f4054156bbe157440d1947233e4fec68705b31e0
slancaster76 <45103771+slancaster76@users.noreply.github.com>
2018-11-27 18:08:58 -0600

    Turned off auto save

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-current-weather',
-  templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  selector: "app-current-weather",
+  template: `
+    <p>current-weather works!</p>
+  `,
+  styleUrls: ["./current-weather.component.css"],
 })
 export class CurrentWeatherComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
