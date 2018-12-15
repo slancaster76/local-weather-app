262a3380bdad5cfa70f8cb2ec54a3273f13ff4d4
slancaster76 <45103771+slancaster76@users.noreply.github.com>
2018-12-15 09:19:52 -0600

    Testing updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { WeatherService } from './weather/weather.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, CurrentWeatherComponent],
+      providers: [WeatherService],
     }).compileComponents();
   }));
 
