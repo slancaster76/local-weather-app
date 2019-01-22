31c624d776a3c0995fe6673497cf6757a785d717
slancaster76 <45103771+slancaster76@users.noreply.github.com>
2019-01-21 19:34:16 -0600

    Added Material support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { MaterialModule } from './material/material.module';
 import { WeatherService } from './weather/weather.service';
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
   providers: [WeatherService],
   bootstrap: [AppComponent],
 })
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
+})
+export class MaterialModule {}
